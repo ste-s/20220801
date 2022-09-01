255db76e238d9265c0934da4c1df412fb31d02e4
ste-s <103716982+ste-s@users.noreply.github.com>
2022-09-01 11:07:40 -0300

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,32 @@
+function processar()
+{
+    var CPF, nome, sexo, email, ifsp, selecao, NE, DV, DA, DMO, DME, T, EX, H, B, ED, Jadinagem, Artesanato, Leitura, Escrita, Musica, Pintura, RedesSociais, Viagens, Series, Academia, Outros;
+    CPF = document.formulario.cpf.value;
+    nome = document.formulario.nome.value;
+    sexo = document.formulario.sexo.value;
+    email = document.formulario.email.value;
+    ifsp = document.formulario.IFSP.checked;
+    selecao = document.formulario.select.value;
+    NE = document.formulario.NE.value;
+    DV = document.formulario.NES.checked;
+    DA = document.formulario.NES.checked;
+    DMO = document.formulario.NES.checked;
+    DME = document.formulario.NES.checked;
+    T = document.formulario.AI.checked;
+    EX = document.formulario.AI.checked;
+    H = document.formulario.AI.checked;
+    B = document.formulario.AI.checked;
+    ED = document.formulario.AI.checked;
+    Jardinagem = document.formulario.H.checked;
+    Artesnato = document.formulario.H.checked;
+    Leitura = document.formulario.H.checked;
+    Escrita = document.formulario.H.checked;
+    Musica = document.formulario.H.checked;
+    Pintura = document.formulario.H.checked;
+    RedesSociais = document.formulario.H.checked;
+    Viagens = document.formulario.H.checked;
+    Series = document.formulario.H.checked;
+    Academia = document.formulario.H.checked;
+    Outros = document.formulario.H.checked;
+}   
+
